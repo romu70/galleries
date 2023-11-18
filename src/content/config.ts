f1968024e6443cde14fc7e547e5c477fcc206a8c
romu70 <r@tisserand.eu>
2023-11-18 17:40:15 +0100

    First version.

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+
+// Define a `type` and `schema` for each collection
+const fonts = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    place: z.string(),
+    pubDate: z.date(),
+    latitude: z.number(),
+    longitude: z.number(),
+    image: z.object({
+      file: image(),
+      alt: z.string(),
+    }),
+  })
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  fonts: fonts,
+};
