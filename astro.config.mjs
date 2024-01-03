867a679f0308b0a2ca8b2084fbed973a5302f057
romu70 <r@tisserand.eu>
2024-01-03 08:47:59 +0100

    added folder for the root

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://romu70.github.io',
+  // base: '/galleries',
+  base: '/galleries',
+})
