ee1fc91698eeb45324de217059b74f23ea771303
romu70 <r@tisserand.eu>
2024-01-19 13:52:34 +0100

    Fixed RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const fonts = await getCollection('fonts');
+  console.log(context.site);
+
   return rss({
-    title: 'Fonts Gallery',
-    description: 'A gallery of nice fonts found and shot in real life',
+    title: 'Galleries',
+    description: 'Galleries of nice things found in real life.',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
+    items: fonts.map((font) => ({
+      title: font.data.image.alt + ", " + font.data.place,
+      pubDate: font.data.pubDate,
+      link: context.site.href
+    })),
   });
 }
