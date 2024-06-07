25ff88c7aa93678316b56dcd64e2d2f8ee42c05f
romu70 <r@tisserand.eu>
2024-06-07 17:47:45 +0200

    Aqstro upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://romu70.github.io',
   base: '/galleries',
+  experimental: {
+    contentCollectionCache: false,
+  },
 })
